d3655d5aecd83eea0eccdfd4d318e4e32e2aa373
Gregor Capuder <capuderg@gmail.com>
2016-07-14 12:37:33 +0200

    Add PHPUnit tests. Basic setup.

---
+++ tests/test-ptcs-plugin-file.php
@@ -0,0 +1,11 @@
+<?php
+
+// Require helpers file containing the helper functions.
+// require_once dirname( __FILE__ ) . '/../inc/class-ocdi-helpers.php';
+
+class PTCS_Plugin_File extends WP_UnitTestCase {
+
+	function test_path_constant() {
+		$this->assertEquals( true, true );
+	}
+}
