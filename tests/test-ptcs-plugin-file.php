876e99a216620bdabf442d37ef5b4098cb68133d
Gregor Capuder <capuderg@gmail.com>
2016-07-14 14:43:19 +0200

    Add tests for PT_Custom_Sidebars class.

--- tests/test-ptcs-plugin-file.php
+++ tests/test-ptcs-plugin-file.php
@@ -3,9 +3,22 @@
 // Require helpers file containing the helper functions.
 // require_once dirname( __FILE__ ) . '/../inc/class-ocdi-helpers.php';
 
-class PTCS_Plugin_File extends WP_UnitTestCase {
+class PT_Custom_Sidebars_Test extends WP_UnitTestCase {
 
-	function test_path_constant() {
-		$this->assertEquals( true, true );
+	/**
+	 * Test the plguin constants. If they are defined.
+	 */
+	function test_plugin_constants() {
+		$this->assertTrue( defined( 'PT_CS_PATH' ), 'Constant PT_CS_PATH is not defined!' );
+		$this->assertTrue( defined( 'PT_CS_URL' ), 'Constant PT_CS_URL is not defined!' );
+		$this->assertTrue( defined( 'PT_CS_VERSION' ), 'Constant PT_CS_VERSION is not defined!' );
+		$this->assertTrue( defined( 'PT_CS_VIEWS_DIR' ), 'Constant PT_CS_VIEWS_DIR is not defined!' );
+	}
+
+	/**
+	 * Test if the main plugin file exists.
+	 */
+	function test_main_plugin_file_exists() {
+		$this->assertFileExists( PT_CS_PATH . 'inc/class-pt-cs-main.php', 'Main plugin file: inc/class-pt-cs-main.php is missing!' );
 	}
 }
