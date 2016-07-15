d40d3f3ecca5e6ee272d33cf86788b1239ea2ed1
Gregor Capuder <capuderg@gmail.com>
2016-07-15 09:18:34 +0200

    Added one more test to the main plugin class.

--- tests/test-ptcs-plugin-file.php
+++ tests/test-ptcs-plugin-file.php
@@ -21,4 +21,12 @@ class PT_Custom_Sidebars_Test extends WP_UnitTestCase {
 	function test_main_plugin_file_exists() {
 		$this->assertFileExists( PT_CS_PATH . 'inc/class-pt-cs-main.php', 'Main plugin file: inc/class-pt-cs-main.php is missing!' );
 	}
+
+	/**
+	 * Test if the load_plugin_textdomain action is registered.
+	 */
+	function test_load_plugin_textdomain() {
+		$instance = PT_Custom_Sidebars::get_instance();
+		$this->assertEquals( 10, has_action( 'plugins_loaded', array( $instance, 'load_plugin_textdomain' ) ) );
+	}
 }
