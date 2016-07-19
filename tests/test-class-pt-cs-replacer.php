948c108de9fb999502210580f2c48ef81c8623c5
Gregor Capuder <capuderg@gmail.com>
2016-07-19 12:38:31 +0200

    Add PHP unit test for has_wrapper_code method.

--- tests/test-class-pt-cs-replacer.php
+++ tests/test-class-pt-cs-replacer.php
@@ -44,6 +44,29 @@ class PT_CS_Replacer_Test extends WP_UnitTestCase {
 		$this->assertArrayHasKey( $custom_sidebars[0]['id'] , $wp_registered_sidebars, 'Custom sidebar should be registered!' );
 	}
 
+	/**
+	 * Test PT_CS_Replacer::has_wrapper_code methods.
+	 *
+	 * @dataProvider PT_CS_Main_Test::custom_siderbars_data_set
+	 */
+	function test_has_wrapper_code( $custom_sidebars ) {
+		$instance = PT_CS_Replacer::get_instance();
+
+		$this->assertTrue( $instance->has_wrapper_code( $custom_sidebars[0] ), 'First sidebar test should have wrapper code!' );
+
+		$this->assertFalse( $instance->has_wrapper_code( $custom_sidebars[1] ), 'Second sidebar test should not have wrapper code!' );
+	}
+
+	/**
+	 * Test PT_CS_Replacer::is_valid_replacement methods.
+	 *
+	 * @dataProvider PT_CS_Main_Test::custom_siderbars_data_set
+	 */
+	function test_is_valid_replacement( $custom_sidebars, $expected ) {
+		$instance = PT_CS_Replacer::get_instance();
+
+	}
+
 
 
 
